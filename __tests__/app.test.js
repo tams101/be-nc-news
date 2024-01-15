aef5d475ab5c4729a42dcaf12fd69311f38a4507
Simon Tam <simontam19@outlook.com>
2024-01-15 15:33:33 +0000

    Refactor code based on feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data/index");
 const fs = require("fs/promises");
-const endpoints = require('../endpoints.json')
+const endpoints = require("../endpoints.json");
 
 afterAll(() => {
   return db.end();
@@ -21,15 +21,16 @@ describe("/api/topics", () => {
       .expect(200)
       .then(({ body }) => {
         const { topics } = body;
-
+        expect(topics).toHaveLength(3);
         topics.forEach((topic) => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
-
-        expect(topics).toHaveLength(3);
       });
   });
+});
+
+describe("Invalid path", () => {
   test("GET: 404 When given in invalid path", () => {
     return request(app)
       .get("/api/banana")
@@ -38,56 +39,63 @@ describe("/api/topics", () => {
         expect(body.msg).toBe("path doesn't exist");
       });
   });
-});
+})
 
 describe("/api", () => {
   test("Provide a description of all the endpoints available", () => {
-    const allEndpoints = endpoints
+    const allEndpoints = endpoints;
 
-    return request(app).get('/api').then(({body}) => {
-      expect(body.endpoints).toEqual(allEndpoints)
-    })
-    
+    return request(app)
+      .get("/api")
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(allEndpoints);
+      });
   });
 });
 
 describe("/api/articles/:article_id", () => {
-  test('GET: 200 sends a single article to the client', () => {
-    return request(app).get('/api/articles/1').expect(200)
-    .then(({body}) => {
-      const {article} = body;
-      console.log(body)
+  test("GET: 200 sends a single article to the client", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
 
-      expect(typeof article.author).toBe('string')
-      expect(typeof article.title).toBe('string')
-      expect(typeof article.article_id).toBe('number')
-      expect(typeof article.body).toBe('string')
-      expect(typeof article.topic).toBe('string')
-      expect(typeof article.created_at).toBe('string')
-      expect(typeof article.votes).toBe('number')
-      expect(typeof article.article_img_url).toBe('string')
-      
-      expect(article.author).toBe('butter_bridge')
-      expect(article.title).toBe('Living in the shadow of a great man')
-      expect(article.article_id).toBe(1)
-      expect(article.body).toBe('I find this existence challenging')
-      expect(article.topic).toBe('mitch')
-      expect(article.created_at).toBe('2020-07-09T20:11:00.000Z')
-      expect(article.votes).toBe(100)
-      expect(article.article_img_url).toBe('https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700')
-      
-    })
+        expect(typeof article.author).toBe("string");
+        expect(typeof article.title).toBe("string");
+        expect(typeof article.article_id).toBe("number");
+        expect(typeof article.body).toBe("string");
+        expect(typeof article.topic).toBe("string");
+        expect(typeof article.created_at).toBe("string");
+        expect(typeof article.votes).toBe("number");
+        expect(typeof article.article_img_url).toBe("string");
+
+        expect(article.author).toBe("butter_bridge");
+        expect(article.title).toBe("Living in the shadow of a great man");
+        expect(article.article_id).toBe(1);
+        expect(article.body).toBe("I find this existence challenging");
+        expect(article.topic).toBe("mitch");
+        expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(article.votes).toBe(100);
+        expect(article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
   });
-  test('GET: 404 an error message is sent to the client when given a valid but non-existent id', () => {
-    return request(app).get('/api/articles/9999').expect(404)
-    .then(({body}) => {
-      expect(body.msg).toBe('article does not exist')
-    })
+  test("GET: 404 an error message is sent to the client when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("article does not exist");
+      });
   });
-  test('GET: 400 an error message is sent to the client when given an invalid id', () => {
-    return request(app).get('/api/articles/orange').expect(400)
-    .then(({body}) => {
-      expect(body.msg).toBe('bad request')
-    })
-  })
-})
+  test("GET: 400 an error message is sent to the client when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/orange")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("bad request");
+      });
+  });
+});
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -6,7 +6,6 @@ exports.getArticleById = (req, res, next) => {
     res.status(200).send({article})
   })
   .catch((err) => {
-    console.log(err)
     next(err)
   })
 }
