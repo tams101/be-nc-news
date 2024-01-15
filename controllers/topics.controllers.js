6e1b12e31eaa19a29904a3f3c596a0b328b1090e
Simon Tam <simontam19@outlook.com>
2024-01-15 11:19:46 +0000

    Complete task 2 get /api/topics

---
+++ controllers/topics.controllers.js
@@ -0,0 +1,7 @@
+const {retrieveAllTopics} = require('../models/topics.models')
+
+exports.getAllTopics = (req, res, next) => {
+  retrieveAllTopics().then((topics) => {
+    res.status(200).send({topics})
+  })
+}
