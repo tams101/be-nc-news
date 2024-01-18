e8d463bd42a0803cb54ff7b92b19857c357c6f27
Simon Tam <simontam19@outlook.com>
2024-01-18 16:19:11 +0000

    Complete task 18 PATCH /api/comments/:comment_id

--- error-handling.js
+++ error-handling.js
@@ -11,11 +11,7 @@ exports.idNotFound = (err, req, res, next) => {
 };
 
 exports.psqlError = (err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "invalid id type" });
-  }
-
-  if (err.code === "23502") {
+  if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "bad request" });
   }
 
