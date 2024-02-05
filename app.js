e513352879f324940e1292ed5a8c88d7c896625a
Simon Tam <simontam19@outlook.com>
2024-02-05 10:11:03 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors")
 const {
   invalidPath,
   psqlError,
@@ -14,6 +14,8 @@ const topicsRouter = require("./routes/topics-router");
 const usersRouter = require("./routes/users-router");
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
